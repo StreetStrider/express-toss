b9b14e38bd4d82648e5d86771d88c47c210b73af
Strider <strange.transistor@gmail.com>
2017-02-22 21:36:24 +0300

    more tests.

--- test/case/Resp.js
+++ test/case/Resp.js
@@ -43,18 +43,46 @@ describe('Resp', () =>
 	{
 		expect(Resp('json', 'body').inspect()).deep.eq([ 200, 'json', 'body' ])
 
-		// expect(Resp('json', 200).inspect()).deep.eq([ 200, 'json', 200 ])
+		expect(Resp('json', 200).inspect()).deep.eq([ 200, 'json', 200 ])
 
-		// expect(Resp('json', { body: true }).inspect())
-		// .deep.eq([ 200, 'json', { body: true } ])
+		expect(Resp('json', { body: true }).inspect())
+		.deep.eq([ 200, 'json', { body: true } ])
 
-		// expect(Resp('json', new Buffer('abc')).inspect())
-		// .deep.eq([ 200, 'json', new Buffer('abc') ])
+		expect(Resp('json', new Buffer('abc')).inspect())
+		.deep.eq([ 200, 'json', new Buffer('abc') ])
 	})
 
 	it('Resp(status, mime, body)', () =>
 	{
-		// expect(Resp(400, 'json', 'body').inspect())
-		// .deep.eq([ 200, 'json', 'body' ])
+		expect(Resp(400, 'json', 'body').inspect())
+		.deep.eq([ 400, 'json', 'body' ])
+
+		expect(Resp(400, 'json', 200).inspect())
+		.deep.eq([ 400, 'json', 200 ])
+
+		expect(Resp(400, 'json', { body: true }).inspect())
+		.deep.eq([ 400, 'json', { body: true } ])
+
+		expect(Resp(400, 'json', new Buffer('abc')).inspect())
+		.deep.eq([ 400, 'json', new Buffer('abc') ])
+	})
+
+	it('Resp(status, Intact, body)', () =>
+	{
+		/* @flow-off */
+		expect(Resp(400, Intact, 'body').inspect())
+		.deep.eq([ 400, Intact, 'body' ])
+
+		/* @flow-off */
+		expect(Resp(400, Intact, 200).inspect())
+		.deep.eq([ 400, Intact, 200 ])
+
+		/* @flow-off */
+		expect(Resp(400, Intact, { body: true }).inspect())
+		.deep.eq([ 400, Intact, { body: true } ])
+
+		/* @flow-off */
+		expect(Resp(400, Intact, new Buffer('abc')).inspect())
+		.deep.eq([ 400, Intact, new Buffer('abc') ])
 	})
 })
